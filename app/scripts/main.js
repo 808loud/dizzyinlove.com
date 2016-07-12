8436898913c0026595ba365cd3cd5798084827d0
David Monson <david@808loud.com>
2016-07-11 23:04:05 -0500

    working Google form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+//
+// $(document).ready(function(){
+// //my email in reverse :)
+//   var s = 'moc.elibomajninbew@htiek';
+//   var e = s.split("").reverse().join("");
+//   $('#email').html('<a href="mailto:'+e+'">'+e+'</a>');
+// });
+
+$(document).ready(function(){
+  setTimeout(function(){
+    // hi in reverse
+    var u = 'ih';
+    // dizzyinlove in reverse
+    var d = 'evolniyzzid'
+    function r(i) {
+      i = i.split("").reverse().join("");
+      return i;
+    }
+    $('.on-human').html('<a href="mailto:'+r(u)+'@'+r(d)+'.com">'+r(u)+'@'+r(d)+'.com</a>');
+  }, 5000);
+});
