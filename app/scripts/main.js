14796c04c1ab0edf354abaef9210f48c52d580f4
David Monson <david@808loud.com>
2016-07-14 02:44:36 -0500

    mostly done with hotels, but internet went down

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,12 +8,21 @@ $(document).ready(function(){
             i = i.split('').reverse().join('');
             return i;
         }
-        $('.on-human').html('<a href="mailto:'+r(u)+'@'+r(d)+'.com">'+r(u)+'@'+r(d)+'.com</a>');
+        $('.on-timeout').html('<a href="mailto:'+r(u)+'@'+r(d)+'.com">'+r(u)+'@'+r(d)+'.com</a>');
     }, 3000);
 
     // Collapse all open Bootstrap collapsable containers
     $(document).on('click',function(){
     	$('.collapse').collapse('hide');
     })
+
+    // Enable Bootstrap v3 tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // TODO flexbox would solve for this
+    var widest = 0;
+    $(".vcard").each(function () {
+      widest = Math.max(widest, $(this).outerWidth());
+    }).width(widest);
 
 });
