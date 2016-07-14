7f67b05e4714997b4930b921ef73a7bc030f317a
David Monson <david@808loud.com>
2016-07-13 23:06:47 -0500

    collapse all .collapse containers on click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-  setTimeout(function(){
-    // hi in reverse
-    var u = 'ih';
-    // dizzyinlove in reverse
-    var d = 'evolniyzzid'
-    function r(i) {
-      i = i.split('').reverse().join('');
-      return i;
-    }
-    $('.on-human').html('<a href="mailto:'+r(u)+'@'+r(d)+'.com">'+r(u)+'@'+r(d)+'.com</a>');
-  }, 3000);
+    setTimeout(function(){
+        // hi in reverse
+        var u = 'ih';
+        // dizzyinlove in reverse
+        var d = 'evolniyzzid'
+        function r(i) {
+            i = i.split('').reverse().join('');
+            return i;
+        }
+        $('.on-human').html('<a href="mailto:'+r(u)+'@'+r(d)+'.com">'+r(u)+'@'+r(d)+'.com</a>');
+    }, 3000);
+
+    // Collapse all open Bootstrap collapsable containers
+    $(document).on('click',function(){
+    	$('.collapse').collapse('hide');
+    })
+
 });
